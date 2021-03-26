aabff8e715a0a3827977697fb5e012ecb702c3e2
diego salgado <diegohumbertosalgadoramirez@gmail.com>
2021-03-26 13:11:06 -0500

    Se añadieron nuevas carpetas y se organiso el orden de los archivos

---
+++ Componentes/banner.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="Recursos/css/main.css">
+    <link rel="stylesheet" href="Recursos/css/skin.css">
+    <link rel="stylesheet" href="Recursos/css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="Recursos/css/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="Recursos/script/index.js"></script>
+    <script src="Recursos/jquery/jquery-3.5.0.min.js"></script>
+</head>
+<body style="overflow-x:hidden" id="wrapper">
+    
+<header>
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="row">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <img src="img/LogoAerolineaMSD.PNG" class="ImagenBannerLogo">
+                    </div>
+
+                    <div id="navbar" class="collapse navbar-collapse navbar-right">
+
+                        <ul class="nav navbar-nav">
+
+                            <li class="active"><a href="#">Inicio</a></li>
+                            <li><a href="">Menu Empleados</a></li>
+                            <li><a href="">Registrarse</a></li>
+                            <li><a href="">Inicio Sesion</a></li>
+                            <li><a href="">Perfil</a></li>
+                        </ul>
+
+                    </div>
+                    <!--/.nav-collapse -->
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!--/.nav-ends -->
+</body>
+</html>
---
+++ Vista/Inicio.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Inicio</title>
+</head>
+<body style="overflow-x:hidden" id="wrapper">
+
+
+    <div id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url('img/aeropuertos-lujo-portada.jpg');"></div>
+                <div class="carousel-caption slide-up">
+                    <h1 class="banner_heading">Providing The <span>Highest </span>Lorem</h1>
+                    <p class="banner_txt">Lorem ipsum dolor sit amet sit legimus copiosae instructior eiut vix denique
+                        fierentis ea saperet inimicu utqui dolor oratio mnesarchum.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="fill" style="background-image:url('img/pixabaynubes-2.jpg');"></div>
+                <div class="carousel-caption slide-up">
+                    <h1 class="banner_heading">Providing The <span>Highest </span>Lorem</h1>
+                    <p class="banner_txt">Lorem ipsum dolor sit amet sit legimus copiosae instructior eiut vix denique
+                        fierentis ea saperet inimicu utqui dolor oratio mnesarchum.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="fill" style="background-image:url('img/Airport inside (1)_2.jpg');"></div>
+                <div class="carousel-caption slide-up">
+                    <h1 class="banner_heading">Providing The <span>Highest </span>Lorem</h1>
+                    <p class="banner_txt">Lorem ipsum dolor sit amet sit legimus copiosae instructior eiut vix denique
+                        fierentis ea saperet inimicu utqui dolor oratio mnesarchum.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="fill" style="background-image:url('img/night-lights-airport-the-plane-wallpaper-preview.jpg');"></div>
+                <div class="carousel-caption slide-up">
+                    <h1 class="banner_heading">Providing The <span>Highest </span>Lorem</h1>
+                    <p class="banner_txt">Lorem ipsum dolor sit amet sit legimus copiosae instructior eiut vix denique
+                        fierentis ea saperet inimicu utqui dolor oratio mnesarchum.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="fill" style="background-image:url('img/plane-1666083_1280.jpg');"></div>
+                <div class="carousel-caption slide-up">
+                    <h1 class="banner_heading">Providing The <span>Highest </span>Lorem</h1>
+                    <p class="banner_txt">Lorem ipsum dolor sit amet sit legimus copiosae instructior eiut vix denique
+                        fierentis ea saperet inimicu utqui dolor oratio mnesarchum.</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"> <i class="fa fa-angle-left"
+                aria-hidden="true"></i>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"> <i
+                class="fa fa-angle-right" aria-hidden="true"></i>
+            <span class="sr-only">Next</span>
+        </a>
+
+    </div>
+
+
+    <section id="login-reg">
+        <!--Linea en main.css 1412-->
+        <div class="container">
+            <!-- Top content -->
+
+            <!--forms-right-icons-->
+            <div class="col-md-12 col-sm-12">
+
+                <!--Linea en form-top en css.main 7682-->
+                <div class="form-top">
+                    <div class="form-top-left">
+                        <div class="ContenedorNavegacionBuscarVuelos">
+                            <nav class="navigation">
+                                <ul class="show">
+                                    <li><a href="">Reservar Vuelos</a></li>
+                                    <li><a href="">Mi Reserva</a></li>
+                                    <li><a href="">Mi Check-in</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <!--Linea en main.css 3308-->
+                    <div class="form-top-right">
+
+                    </div>
+                </div>
+                <div class="form-bottom">
+                    <form role="form" action="" class="login-form">
+
+
+                        <div class="input-group form-group">
+                            <span id="basic-addon1">
+                                <h7 class="textoh7">Solo Ida</h7>
+                                <input type="checkbox" id="chk1" name="checkbox" class="form"
+                                    aria-describedby="basic-addon1">
+                            </span>
+
+                        </div>
+
+
+                        <div class="ContenedorComboBoxOrigen">
+                            <div class="input-group form-group">
+                                <span class="input-group-addon" id="basic-addon1"><i
+                                        class="fas fa-plane-departure"></i></span>
+                                <select type="text" class="form-control" id="CmbOrigen" aria-describedby="basic-addon1">
+                                    <option>Seleccione el Origen</option>
+
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="ContenedorComboBoxDestino">
+                            <div class="input-group form-group">
+
+                                <span class="input-group-addon" id="basic-addon1"><i
+                                        class="fas fa-plane-arrival"></i></span>
+                                <select type="text" class="form-control" id="CmbDestiono"
+                                    aria-describedby="basic-addon1">
+                                    <option>Seleccione el Destino</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="ContenedorFechaSalida">
+                            <h6>Seleccione la Fecha de Salida</h6>
+                            <div class="input-group form-group">
+                                <span class="input-group-addon" id="basic-addon1"><i
+                                        class="fas fa-calendar-alt"></i></span>
+                                <input type="date" class="form-control" id="FechaSalida"
+                                    aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+
+                        <div class="ContenedorFechaRegreso">
+                            <h6>Seleccione la Fecha de Regreso</h6>
+                            <div class="input-group form-group">
+                                <span class="input-group-addon" id="basic-addon1"><i
+                                        class="fas fa-calendar-alt"></i></span>
+                                <input type="date" class="form-control" id="FechaRegreso"
+                                    aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+
+                        <div class="ContenedorCantidadPasajeros">
+                            <div class="input-group form-group">
+                                <span class="input-group-addon" id="basic-addon1"><i class="fa fa-user"></i></span>
+                                <select type="text" class="form-control" id="CmbPasajeros"
+                                    aria-describedby="basic-addon1">
+                                    <option>Seleccione la cantidad de pasajeros</option>
+                                    <option>Pasajeros 1</option>
+                                    <option>Pasajeros 2</option>
+                                    <option>Pasajeros 3</option>
+                                    <option>Pasajeros 4</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="ContenedorTipoVuelos">
+                            <div class="input-group form-group">
+                                <span class="input-group-addon" id="basic-addon1"><i
+                                        class="fas fa-globe-americas"></i></span>
+                                <select type="text" class="form-control" id="CmbTipoVuelo"
+                                    aria-describedby="basic-addon1">
+                                    <option>Seleccione el tipo de vuelo</option>
+                                    <option>Nacional</option>
+                                    <option>Internacional</option>
+
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="ContenedorBotonBuscarVuelos">
+                            <button type="button" id="btnRegistrar" class="boton_personalizado">Buscar Vuelo</button>
+                        </div>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="section-heading text-center">
+                    <div class="col-md-12 col-xs-12">
+                        <h1>Te contamos lo que pasa en<span> Aerolinea MSD</span></h1>
+                        <p class="subheading">Disfruta nuevos productos y servicios..</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 portfolio-item">
+                    <div class="portfolio-one">
+                        <div class="portfolio-head">
+                            <div class="portfolio-img"><img alt="" src="img/value_proposition_slider1.jpg"></div>
+                            
+                        </div>
+                        <!-- End portfolio-head -->
+                        <div class="portfolio-content">
+                            <h5 class="title">Al preparar tu viaje</h5>
+                            <p>Lo que necesitas saber con anticipación.</p>
+                        </div>
+                        <!-- End portfolio-content -->
+                    </div>
+                    <!-- End portfolio-item -->
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 portfolio-item">
+                    <div class="portfolio-one">
+                        <div class="portfolio-head">
+                            <div class="portfolio-img"><img alt="" src="img/value_proposition_slider2.jpg"></div>
+                           
+                        </div>
+                        <!-- End portfolio-head -->
+                        <div class="portfolio-content">
+                            <h5 class="title">En el aeropuerto</h5>
+                            <p>Hacemos fácil el día de tu viaje.</p>
+                        </div>
+                        <!-- End portfolio-content -->
+                    </div>
+                    <!-- End portfolio-item -->
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 portfolio-item">
+                    <div class="portfolio-one">
+                        <div class="portfolio-head">
+                            <div class="portfolio-img"><img alt="" src="img/value_proposition_slider3.jpg"></div>
+                            
+                        </div>
+                        <!-- End portfolio-head -->
+                        <div class="portfolio-content">
+                            <h5 class="title">Durante el vuelo</h5>
+                            <p>Relájate y disfruta de nuestro servicio a bordo.</p>
+                        </div>
+                        <!-- End portfolio-content -->
+                    </div>
+                    <!-- End portfolio-item -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 portfolio-item">
+                    <div class="portfolio-one">
+                        <div class="portfolio-head">
+                            <div class="portfolio-img"><img alt="" src="img/banner-secundario-pasajero-10-men.jpg"></div>
+                           
+                        </div>
+                        <!-- End portfolio-head -->
+                        <div class="portfolio-content">
+                            <h5 class="title">Beneficios para tu prueba PCR</h5>
+                            <p>Trabajamos en alianza con laboratorios médicos para la toma de tu prueba COVID-19.</p>
+                        </div>
+                        <!-- End portfolio-content -->
+                    </div>
+                    <!-- End portfolio-item -->
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 portfolio-item">
+                    <div class="portfolio-one">
+                        <div class="portfolio-head">
+                            <div class="portfolio-img"><img alt="" src="img/Banner-secundario-booking-rentalcars.jpg"></div>
+                            
+                        </div>
+                        <!-- End portfolio-head -->
+                        <div class="portfolio-content">
+                            <h5 class="title">¿Necesitas completar tu viaje?</h5>
+                            <p>Aprovecha descuentos exclusivos con Booking.com y Rentalcars.com para tu alojamiento y alquiler de autos.</p>
+                        </div>
+                        <!-- End portfolio-content -->
+                    </div>
+                    <!-- End portfolio-item -->
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 portfolio-item">
+                    <div class="portfolio-one">
+                        <div class="portfolio-head">
+                            <div class="portfolio-img"><img alt="" src="img/banner-secundario-nueva-app-mujer-esp.jpg"></div>
+                            
+                        </div>
+                        <!-- End portfolio-head -->
+                        <div class="portfolio-content">
+                            <h5 class="title">¡Nueva MyAerolineaMSD app!</h5>
+                            <p>Compra y gestiona tus vuelos desde el cel disfrutando un diseño renovado, más rápido y simple.</p>
+                        </div>
+                        <!-- End portfolio-content -->
+                    </div>
+                    <!-- End portfolio-item -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 portfolio-item">
+                    <div class="portfolio-one">
+                        <div class="portfolio-head">
+                            <div class="portfolio-img"><img alt="" src="img/Banner-secundario-parajero-tapabocas-v4.jpg"></div>
+                            
+                        </div>
+                        <!-- End portfolio-head -->
+                        <div class="portfolio-content">
+                            <h5 class="title">Requisitos para tu viaje</h5>
+                            <p>Infórmate sobre las medidas que debes cumplir para ingresar a tu destino: pruebas de COVID-19, cuarentenas y más.</p>
+                        </div>
+                        <!-- End portfolio-content -->
+                    </div>
+                    <!-- End portfolio-item -->
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 portfolio-item">
+                    <div class="portfolio-one">
+                        <div class="portfolio-head">
+                            <div class="portfolio-img"><img alt="" src="img/b-miamibeach-10092020.jpg"></div>
+                            
+                        </div>
+                        <!-- End portfolio-head -->
+                        <div class="portfolio-content">
+                            <h5 class="title">Viaje a EE. UU.</h5>
+                            <p>Planifique su próximo viaje con nuestras tarifas bajas a EE. UU.</p>
+                        </div>
+                        <!-- End portfolio-content -->
+                    </div>
+                    <!-- End portfolio-item -->
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 portfolio-item">
+                    <div class="portfolio-one">
+                        <div class="portfolio-head">
+                            <div class="portfolio-img"><img alt="" src="img/b2-MXinstallments-couple.jpg"></div>
+                            
+                        </div>
+                        <!-- End portfolio-head -->
+                        <div class="portfolio-content">
+                            <h5 class="title">Meses sin intereses</h5>
+                            <p>Su siguiente viaje en cómodas mensualidades con Tarjetas y Bancos participantes</p>
+                        </div>
+                        <!-- End portfolio-content -->
+                    </div>
+                    <!-- End portfolio-item -->
+                </div>
+            </div>
+
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+</body>
+
+</html>
