be46156b90a47fdb1fffc800c282cba22bbde116
“mateogomezing” <mateogomezramirez@hotmail.com>
2021-04-02 16:47:20 -0500

    se agrego la clase empleado

---
+++ Modelo/clsEmpleado.php
@@ -0,0 +1,18 @@
+<?php
+class clsEmpleado {
+    private $id;
+    private $nombres;
+    private $apellidos;
+    private $cedula;
+    private $correo;
+    private $telefonoCelular;
+    private $contrasena;
+    private $estado;
+    private $descripcion;
+
+    function __construct($id, $nombres, $apellidos, $cedula,$correo,$telefonoCelular,$contrasena,$estado,$descripcion) {
+        
+
+
+    }
+}
