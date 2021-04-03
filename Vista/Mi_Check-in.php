8361ff5198f19cd02790bab8cfa91a6b89fc9cb3
diego salgado <diegohumbertosalgadoramirez@gmail.com>
2021-04-03 03:12:24 -0500

    Se agrego la Vista del Check-in

---
+++ Vista/Mi_Check-in.php
@@ -0,0 +1,256 @@
+<?php
+include_once 'layouts/header_Check-in.php';
+?>
+<title>Mi Check-in</title>
+<?php
+include_once 'layouts/nav_Check-in.php';
+?>
+
+
+<section class="ContenedorFormularioReserva">
+    <div class="panel panel-primary">
+
+        <div class="panel-heading">
+            <h4 class=" estiloTextReserva">Mi Check-in</h4>
+        </div>
+        <div class="panel-body">
+            <form action="" class="form-horizontal">
+                <div class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="card card-success">
+                                    <div class="card-header">
+                                        <h3 class="card-title">Pasajero</h3>
+                                        <hr class="LineaHorizontal">
+                                    </div>
+                                    <div class="card-body">
+
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Nombre</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtNombre" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Apellido</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtApellido" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Cedula</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtCedula" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Telefono</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtTelefono" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Correo</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtCorreo" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Genero</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtGenero" class="form-control">
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="content ContenedorFormularioInformacionReserva">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="card card-success">
+                                    <div class="card-header">
+                                        <h3 class="card-title">Informacion de Reserva</h3>
+                                        <hr class="LineaHorizontal">
+                                    </div>
+                                    <div class="card-body">
+
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Estado</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtEstado" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Hora Salida</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtHoraSalida" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Silla Seleccionada</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtSilla" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Origen</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtOrigen" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Destino</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtDestino" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">FechaLlegada</label>
+                                            <div class="col-sm-4">
+                                                <input type="date" id="txtFechaLlegada" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">FechaSalida</label>
+                                            <div class="col-sm-4">
+                                                <input type="date" id="txtFechaSalida" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Tipo de Vuelo</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtTipo Vuelo" class="form-control">
+                                            </div>
+
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="" class="col-sm-2 col-form-label">Estado Vuelo</label>
+                                            <div class="col-sm-4">
+                                                <input type="text" id="txtEstado Vuelo" class="form-control">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <div class="card card-success">
+                                <div class="card-header">
+                                    <h3 class="card-title">Vuelo</h3>
+                                    <hr class="LineaHorizontal">
+                                </div>
+                                <div class="card-body">
+
+
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-2 col-form-label">Numero de Vuelo</label>
+                                        <div class="col-sm-4">
+                                            <input type="text" id="txtEstado" class="form-control">
+                                        </div>
+
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-2 col-form-label">Nombre Aerolinea</label>
+                                        <div class="col-sm-4">
+                                            <input type="text" id="txtHoraSalida" class="form-control">
+                                        </div>
+
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-2 col-form-label">Estado del Vuelo</label>
+                                        <div class="col-sm-4">
+                                            <input type="text" id="txtSilla" class="form-control">
+                                        </div>
+
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-2 col-form-label">Placa del Avion</label>
+                                        <div class="col-sm-4">
+                                            <input type="text" id="txtOrigen" class="form-control">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="" class="col-sm-2 col-form-label">Destino</label>
+                                        <div class="col-sm-4">
+                                            <input type="text" id="txtDestino" class="form-control">
+                                        </div>
+
+                                    </div>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer row">
+                    <p class="ColorTextoModalIniciarSesion estiloTextReserva">Si quere puede descargar el pasaporte del
+                        buelo dando click en el
+                        boton de imprimir Check-in
+                    </p>
+                    <div class="offset-sm-2 col-sm-12 float-right">
+                        <button type="button" class="btn btn-danger">imprimir Check-in</button>
+                    </div>
+                </div>
+
+                <div class="modal-footer row">
+
+                    <p class="ColorTextoModalIniciarSesion estiloTextReserva">Si quiere validar el chack-in debe dar
+                        click en el boton
+                        de Aceptar para que pueda entar en la sala de abordaje del Vuelo
+                    </p>
+                    <div class="offset-sm-2 col-sm-12 float-right">
+                        <button type="button" class="btn btn-danger">Aceptar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+</section>
+
+<?php
+include_once 'layouts/footer_Check-in.php';
+?>
