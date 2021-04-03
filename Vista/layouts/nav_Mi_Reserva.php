52edd7e383e9fc667e223545d5d07c10e5cd4d46
diego salgado <diegohumbertosalgadoramirez@gmail.com>
2021-04-03 02:23:51 -0500

    Se agrego el HTML de la vista de mi reserva

--- Vista/layouts/nav_Mi_Reserva.php
+++ Vista/layouts/nav_Mi_Reserva.php
@@ -2,14 +2,14 @@
     <title>Document</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="Recursos/css/main.css">
-    <link rel="stylesheet" href="Recursos/css/skin.css">
-    <link rel="stylesheet" href="Recursos/css/estilos.css">
+    <link rel="stylesheet" href="../Recursos/css/main.css">
+    <link rel="stylesheet" href="../Recursos/css/skin.css">
+    <link rel="stylesheet" href="../Recursos/css/estilos.css">
     <link rel="stylesheet" type="text/css" href="Recursos/css/css/all.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="Recursos/script/index.js"></script>
-    <script src="Recursos/jquery/jquery-3.5.0.min.js"></script>
+    <script src="../Recursos/script/index.js"></script>
+    <script src="../Recursos/jquery/jquery-3.5.0.min.js"></script>
 </head>
 <body style="overflow-x:hidden" id="wrapper">
     
@@ -25,7 +25,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <img src="img/LogoAerolineaMSD.PNG" class="ImagenBannerLogo">
+                        <img src="../img/LogoAerolineaMSD.PNG" class="ImagenBannerLogo">
                     </div>
 
                     <div id="navbar" class="collapse navbar-collapse navbar-right">
@@ -33,8 +33,6 @@
                         <ul class="nav navbar-nav">
 
                             <li class="active"><a href="#">Inicio</a></li>
-                            <li><a href="" data-target="#Registrarse" data-toggle="modal" type="button">Registrarse</a></li>
-                            <li><a href="" data-target="#Iniciar_Sesion" data-toggle="modal" type="button">Inicio Sesion</a></li>
                             <li><a href="">Informacion</a></li>
                             <li><a href="">Te ayudamos</a></li>
                         </ul>
