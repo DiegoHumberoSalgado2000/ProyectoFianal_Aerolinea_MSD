afcbb2abd8dddf957c036e5011278565fba56c06
diego salgado <diegohumbertosalgadoramirez@gmail.com>
2021-04-03 03:41:08 -0500

    Se agrego los layouts de la vista de Lista de Vuelos

---
+++ Vista/Lista_Vuelos.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'layouts/header_Pagina_Inicio.php'
+?>
+<title>Lista de Vuelos</title>
+<?php
+include_once 'layouts/nav_Pagina_Inicio.php';
+?>
+
+
+
+
+
+
+<?php
+include_once 'layouts/footer_Pagina_Inicio.php';
+?>
