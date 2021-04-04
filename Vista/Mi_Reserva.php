3b8472448b14c2456c6e2bb7d36107e935e01b52
diego salgado <diegohumbertosalgadoramirez@gmail.com>
2021-04-04 15:07:23 -0500

    Se rediseño las vistas de mi reserva y de check-in

--- Vista/Mi_Reserva.php
+++ Vista/Mi_Reserva.php
@@ -14,163 +14,144 @@ include_once 'layouts/nav_Mi_Reserva.php';
         </div>
         <div class="panel-body">
             <form action="" class="form-horizontal">
-                <div class="content">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-7">
-                                <div class="card card-success">
-                                    <div class="card-header">
-                                        <h3 class="card-title">Pasajero</h3>
-                                        <hr class="LineaHorizontal">
-                                    </div>
-                                    <div class="card-body">
+
+                <div class="panel  panel-danger">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Pasajero</h3>
+                    </div>
+                    <div class="panel-body">
 
 
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Nombre</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtNombre" class="form-control">
-                                            </div>
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Nombre</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtNombre" class="form-control">
+                            </div>
 
-                                        </div>
+                        </div>
 
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Apellido</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtApellido" class="form-control">
-                                            </div>
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Apellido</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtApellido" class="form-control">
+                            </div>
 
-                                        </div>
+                        </div>
 
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Cedula</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtCedula" class="form-control">
-                                            </div>
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Cedula</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtCedula" class="form-control">
+                            </div>
 
-                                        </div>
+                        </div>
 
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Telefono</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtTelefono" class="form-control">
-                                            </div>
-                                        </div>
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Telefono</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtTelefono" class="form-control">
+                            </div>
+                        </div>
 
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Correo</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtCorreo" class="form-control">
-                                            </div>
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Correo</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtCorreo" class="form-control">
+                            </div>
 
-                                        </div>
+                        </div>
 
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Genero</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtGenero" class="form-control">
-                                            </div>
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Genero</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtGenero" class="form-control">
+                            </div>
 
-                                        </div>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="panel  panel-danger">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Informacion de la Reserva</h3>
+                    </div>
+                    <div class="panel-body">
 
-                                    </div>
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Estado</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtEstado" class="form-control">
+                            </div>
 
-                                </div>
+                        </div>
 
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Hora Salida</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtHoraSalida" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Silla Seleccionada</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtSilla" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Origen</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtOrigen" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Destino</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtDestino" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">FechaLlegada</label>
+                            <div class="col-sm-5">
+                                <input type="date" id="txtFechaLlegada" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">FechaSalida</label>
+                            <div class="col-sm-5">
+                                <input type="date" id="txtFechaSalida" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Tipo de Vuelo</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtTipo Vuelo" class="form-control">
+                            </div>
+
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="" class="col-sm-2 col-form-label">Estado Vuelo</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="txtEstado Vuelo" class="form-control">
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="content ContenedorFormularioInformacionReserva">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-7">
-                                <div class="card card-success">
-                                    <div class="card-header">
-                                        <h3 class="card-title">Informacion de Reserva</h3>
-                                        <hr class="LineaHorizontal">
-                                    </div>
-                                    <div class="card-body">
 
-
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Estado</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtEstado" class="form-control">
-                                            </div>
-
-                                        </div>
-
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Hora Salida</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtHoraSalida" class="form-control">
-                                            </div>
-
-                                        </div>
-
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Silla Seleccionada</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtSilla" class="form-control">
-                                            </div>
-
-                                        </div>
-
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Origen</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtOrigen" class="form-control">
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Destino</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtDestino" class="form-control">
-                                            </div>
-
-                                        </div>
-
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">FechaLlegada</label>
-                                            <div class="col-sm-4">
-                                                <input type="date" id="txtFechaLlegada" class="form-control">
-                                            </div>
-
-                                        </div>
-
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">FechaSalida</label>
-                                            <div class="col-sm-4">
-                                                <input type="date" id="txtFechaSalida" class="form-control">
-                                            </div>
-
-                                        </div>
-
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Tipo de Vuelo</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtTipo Vuelo" class="form-control">
-                                            </div>
-
-                                        </div>
-
-                                        <div class="form-group row">
-                                            <label for="" class="col-sm-2 col-form-label">Estado Vuelo</label>
-                                            <div class="col-sm-4">
-                                                <input type="text" id="txtEstado Vuelo" class="form-control">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div class="modal-footer row">
-                    <p class="ColorTextoModalIniciarSesion estiloTextReserva">Si quiere cancelar su reserva solo tiene como plaso minimo de 8 dias antes del Vuelo</p>
+                    <p class="ColorTextoModalIniciarSesion estiloTextReserva">Si quiere cancelar su reserva solo tiene
+                        como plaso minimo de 8 dias antes del Vuelo</p>
                     <br>
                     <div class="offset-sm-2 col-sm-12 float-right">
                         <button type="button" class="btn btn-danger">Cancelar Reserva</button>
