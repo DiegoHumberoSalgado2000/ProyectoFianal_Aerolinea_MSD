602fb452e2a1fa5b552b40f08b1ba5a71bc5be50
SANTIAGO POSADA H <santi.posada.a@hotmail.com>
2021-04-20 00:10:32 -0500

    Se documenta el codigo de gestionAvion.php como también de avionDAO.php y gestionAvion.js adicionalmente modifico la función de consultar del avionDAO.php que hace dos sub consultas dentro de la misma para en vez de mostrar el id de color y marca muestre sus respectivos nombres. y con ello modificar el consultar de gestionAvion.js se modifica a info[f].nombreMarca y info[f].nombreColor.

--- DAO/avionDAO.php
+++ DAO/avionDAO.php
@@ -10,34 +10,50 @@ class avionDAO{
         $this->objCon = new clsConexion();
         $this->con = $this->objCon->conectar();
     }
-
+    /**
+     *Función utilizada para guardar un avión, entra como parametro un objeto de tipo avión
+     */
     public function guardar(clsAvion $obj) {
         $sql = "INSERT INTO avion(placa,id_color,id_marca,descripcion,estado) VALUES ('" . $obj->getPlaca() . "','" . $obj->getIdColor() . "','" . $obj->getIdMarca() . "','" . $obj->getDescripcion() . "','disponible')";
         $this->objCon->ExecuteTransaction($sql);
     }
+    /**
+     *Función utilizada para buscar un avión, entra como parametro un objeto de tipo avión
+     */
 
     public function buscar(clsAvion $obj) {
         $sql = "SELECT id,placa,id_color,id_marca,descripcion from avion where placa='" . $obj->getPlaca() . "' and estado='disponible'";
         $this->objCon->Execute($sql);
     }
+    /**
+     *Función utilizada para buscar un fabricante por el id de la marca, entra como parametro un objeto de tipo avión
+     */
 
     public function buscarFabricantePorIdMarca(clsAvion $obj) {
         $sql ="SELECT f.id,f.nombre from fabricante f JOIN marca m on m.id_fabricante=f.id where m.id='" . $obj->getIdMarca() . "'";
         $this->objCon->Execute($sql);
     }
-
+    /**
+     *Función utilizada para eliminar un avión, en este caso realiza un actualizar el cual cambia el estado a 'no disponible'
+     */
     public function eliminar(clsAvion $obj) {
         $sql = "UPDATE avion set estado='no disponible' where id='" . $obj->getId() . "'";
         $this->objCon->ExecuteTransaction($sql);
     }
-
+    /**
+     *Función utilizada para modificar un avión, entra como parametro un objeto de tipo avión
+     */
     public function modificar(clsAvion $obj) {
         $sql = "UPDATE avion set id_color='" . $obj->getIdColor() . "',id_marca='" . $obj->getIdMarca() . "',descripcion='" . $obj->getDescripcion() . "' where id='" . $obj->getId() . "'";
         $this->objCon->ExecuteTransaction($sql);
     }
 
+    /**
+     *Funcion utilizada para para listar todos lo aviones con el estado en 'disponible'
+     */
+
     public function listar() {
-        $sql = "SELECT placa,id_color,id_marca,descripcion,estado from avion where estado='disponible'";
+        $sql = "SELECT placa,(SELECT nombre from color where id=id_color) as nombreColor,(SELECT nombre from marca where id=id_marca) as nombreMarca,descripcion,estado from avion where estado='disponible'";
         $this->objCon->Execute($sql);
     }
 
