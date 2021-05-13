1ca2a6e65f1213dafaac5e41a1f7c3895601a652
diego salgado <diegohumbertosalgadoramirez@gmail.com>
2021-05-12 20:51:21 -0500

    Se modifico el DAO de histrial pagos

--- DAO/HistorialPagoDAO.php
+++ DAO/HistorialPagoDAO.php
@@ -29,13 +29,13 @@ class HistorialPagoDAO {
      * entra como parametro un objeto de tipo pago
      */
     public function guardar(ClsHistorialPago $obj) {
-        $sql = "INSERT INTO historial_pago(id,id_reserva,total_pagar,estado,descripcion)VALUES('" . $obj->getId() . "','" . $obj->getId_Reserva() . "','" . $obj->getTotal_Pagos() . "','disponible','ninguno')";
+        $sql = "INSERT INTO historial_pago(id,id_reserva,total_pagar,estado,targeta_credito,mes_vencimiento,opcion_pago,Avencimiento,numero_targeta,numero_verificacion,descripcion)VALUES('" . $obj->getId() . "','" . $obj->getId_Reserva() . "','" . $obj->getTotal_Pagos() . "','disponible','" . $obj->getTargeta_creditp() . "','" . $obj->getMes_vencimiento() . "','" . $obj->getOpcion_pago() . "','" . $obj->getAvencimiento() . "','" . $obj->getNumero_targeta() . "','" . $obj->getNumero_verificacion() . "','ninguno')";
         $this->objCon->ExecuteTransaction($sql);
     }
 
-     /**
-     *Funcion utilizada para listar todos los pagos del los pasageros
-     *con el estado en 'disponible'
+    /**
+     * Funcion utilizada para listar todos los pagos del los pasageros
+     * con el estado en 'disponible'
      */
     public function Lista() {
         $sql = "SELECT id,id_reserva,total_pagar,estado,descripcion from historial_pago where estado='disponible'";
