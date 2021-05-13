115b1859ee9ffb124787784a49fbd2f56eddfe71
diego salgado <diegohumbertosalgadoramirez@gmail.com>
2021-05-12 20:12:59 -0500

    Se agrego mas atributos a la clase historial pago

--- Modelo/ClsHistorialPago.php
+++ Modelo/ClsHistorialPago.php
@@ -5,13 +5,25 @@ class ClsHistorialPago{
     private $id_Reserva;
     private $total_Pagos;
     private $estado;
+    private $targeta_creditp;
+    private $mes_vencimiento;
+    private $opcion_pago;
+    private $Avencimiento;
+    private $numero_targeta;
+    private $numero_verificacion;
     private $descripcion;
     
-    function __construct($id, $id_Reserva, $total_Pagos, $estado, $descripcion) {
+    function __construct($id, $id_Reserva, $total_Pagos, $estado, $targeta_creditp, $mes_vencimiento, $opcion_pago, $Avencimiento, $numero_targeta, $numero_verificacion, $descripcion) {
         $this->id = $id;
         $this->id_Reserva = $id_Reserva;
         $this->total_Pagos = $total_Pagos;
         $this->estado = $estado;
+        $this->targeta_creditp = $targeta_creditp;
+        $this->mes_vencimiento = $mes_vencimiento;
+        $this->opcion_pago = $opcion_pago;
+        $this->Avencimiento = $Avencimiento;
+        $this->numero_targeta = $numero_targeta;
+        $this->numero_verificacion = $numero_verificacion;
         $this->descripcion = $descripcion;
     }
 
@@ -29,6 +41,30 @@ class ClsHistorialPago{
 
     function getEstado() {
         return $this->estado;
+    }
+
+    function getTargeta_creditp() {
+        return $this->targeta_creditp;
+    }
+
+    function getMes_vencimiento() {
+        return $this->mes_vencimiento;
+    }
+
+    function getOpcion_pago() {
+        return $this->opcion_pago;
+    }
+
+    function getAvencimiento() {
+        return $this->Avencimiento;
+    }
+
+    function getNumero_targeta() {
+        return $this->numero_targeta;
+    }
+
+    function getNumero_verificacion() {
+        return $this->numero_verificacion;
     }
 
     function getDescripcion() {
@@ -49,6 +85,30 @@ class ClsHistorialPago{
 
     function setEstado($estado){
         $this->estado = $estado;
+    }
+
+    function setTargeta_creditp($targeta_creditp){
+        $this->targeta_creditp = $targeta_creditp;
+    }
+
+    function setMes_vencimiento($mes_vencimiento){
+        $this->mes_vencimiento = $mes_vencimiento;
+    }
+
+    function setOpcion_pago($opcion_pago){
+        $this->opcion_pago = $opcion_pago;
+    }
+
+    function setAvencimiento($Avencimiento){
+        $this->Avencimiento = $Avencimiento;
+    }
+
+    function setNumero_targeta($numero_targeta){
+        $this->numero_targeta = $numero_targeta;
+    }
+
+    function setNumero_verificacion($numero_verificacion){
+        $this->numero_verificacion = $numero_verificacion;
     }
 
     function setDescripcion($descripcion){
