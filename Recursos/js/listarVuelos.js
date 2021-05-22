cdc25ac8298546d396369585a726e955c60cc2bd
SANTIAGO POSADA H <santi.posada.a@hotmail.com>
2021-05-22 16:20:57 -0500

    se manda el json encriptado del inicio a listar vuelos

--- Recursos/js/listarVuelos.js
+++ Recursos/js/listarVuelos.js
@@ -1,41 +1,61 @@
 $(document).ready(function () {
 
-    listarTabla();
-    
-    
+    listarTabla(datos);
+
+
     });
-    
-    function listarTabla() {
-    
-        var json=$("#txtJson").val();
-    
-        var info = JSON.parse(json);
-    
-        var arreglo = JSON.parse(info.data);
-    
-        if (info.msj === "Success") {
-        var lista = "";
-        if (arreglo.length > 0) {
-            for (f = 0; f < arreglo.length; f++) {
-                lista = lista + "<tr>";
-                lista = lista + "<td>" + arreglo[f].placa + "</td>";
-                lista = lista + "<td>" + arreglo[f].ubicacionSalida + "</td>";
-                lista = lista + "<td>" + arreglo[f].fecha_salida + "</td>";
-                lista = lista + "<td>" + arreglo[f].ubicacionLlegada + "</td>";
-                lista = lista + "<td>" + arreglo[f].fecha_llegada + "</td>";
-                lista = lista + "<td>" + arreglo[f].precio + "</td>";
-                lista = lista + "<td>" + arreglo[f].descripcion + "</td>";
-                lista = lista + "<td>" + arreglo[f].estado + "</td>";
-                lista = lista + "<td>" +   '<a href=http://localhost/Aerolinea_MSD/Vista/Seleccion_Asientos.php?res='+arreglo[f].id+' type="button" id="Ir" class="btn btn-danger" >Seleccionar</a>' + "</td>";
-                lista = lista + "</tr>";
+
+    //var CryptoJS = require("crypto-js");
+
+    function Encrypt(word, key = '1239873697412580') {
+        let encJson = CryptoJS.AES.encrypt(JSON.stringify(word), key).toString()
+        let encData = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(encJson))
+        return encData
+    }
+
+    function Decrypt(word, key = '1239873697412580') {
+        let decData = CryptoJS.enc.Base64.parse(word).toString(CryptoJS.enc.Utf8)
+        let bytes = CryptoJS.AES.decrypt(decData, key).toString(CryptoJS.enc.Utf8)
+        return JSON.parse(bytes)
+    }
+
+
+    function listarTabla(datos) {
+
+        //var datos=$("#txtDatos").val();
+        try {
+            var json=Decrypt(datos);
+            var info = JSON.parse(json);
+            var arreglo = JSON.parse(info.data);
+
+            if (info.msj === "Success") {
+            var lista = "";
+            if (arreglo.length > 0) {
+                for (f = 0; f < arreglo.length; f++) {
+                    lista = lista + "<tr>";
+                    lista = lista + "<td>" + arreglo[f].placa + "</td>";
+                    lista = lista + "<td>" + arreglo[f].ubicacionSalida + "</td>";
+                    lista = lista + "<td>" + arreglo[f].fecha_salida + "</td>";
+                    lista = lista + "<td>" + arreglo[f].ubicacionLlegada + "</td>";
+                    lista = lista + "<td>" + arreglo[f].fecha_llegada + "</td>";
+                    lista = lista + "<td>" + arreglo[f].precio + "</td>";
+                    lista = lista + "<td>" + arreglo[f].descripcion + "</td>";
+                    lista = lista + "<td>" + arreglo[f].estado + "</td>";
+                    lista = lista + "<td>" +   '<a href=http://localhost/Aerolinea_MSD/Vista/Seleccion_Asientos.php?res='+arreglo[f].id+' type="button" id="Ir" class="btn btn-danger" >Seleccionar</a>' + "</td>";
+                    lista = lista + "</tr>";
+                    }
+
+                    $("#tablaVuelos").html(lista);
+                }else{
+                    $("#tablaVuelos").html("<b>No se encuentra informacion</b>");
                 }
-    
-                $("#tablaVuelos").html(lista);
-            }else{
-                $("#tablaVuelos").html("<b>No se encuentra informacion</b>");
+            } else {
+                alert("No se encuentra vuelo con esos datos");
             }
-        } else {
-            alert("No se encuentra vuelo con esos datos");
+
+        } catch (error) {
+            alert("No altere la direccion url");
         }
-    
+
+
     }
