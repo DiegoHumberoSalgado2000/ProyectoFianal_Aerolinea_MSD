ba458873968d3665e574998084aae0845ef6f0df
diego salgado <diegohumbertosalgadoramirez@gmail.com>
2021-06-08 11:56:43 -0500

    Se agrego el Controlador de Historial Pago Reserva

---
+++ Controlador/gestionHistorialPagoReserva.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require '../DAO/HistorialPagoReservaDAO.php';
+
+$type = isset($_REQUEST['type']) ? $_REQUEST['type'] : "";
+
+$historialPagoReservaDao=new HistorialPagoReservaDAO();
+
+
+switch ($type) {
+    case "list":
+    $historialPagoReservaDao->listar();
+        break;
+
+}
+
